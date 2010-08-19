1d6b08dacdb2503e6dcff025d38771d13ca2dea7
daniel <daniel@orgim.net>
2010-08-19 23:55:32 +0200

    merge failed :/

--- src/core/reflections/sphere_map_reflections.py
+++
@@ -1,6 +0,0 @@
-# -*- coding: UTF-8 -*-
-'''
-Created on 31.07.2010
-
-@author: Daniel Beßler <daniel@orgizm.net>
-'''
--- src/utils/package_config.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: UTF-8 -*-
-'''
-Created on 27.07.2010
-
-@author: Daniel Beßler <daniel@orgizm.net>
-'''
-
-# set to true for releases
-RELEASE_VERSION = True
